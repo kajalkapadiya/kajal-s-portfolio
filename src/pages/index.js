8a8aef09719345a1a8f000bd13278aa1c28094fb
kajalkapadiya <kajalkapadiya18@gmail.com>
2023-08-10 22:24:51 +0530

    solve some margin problem

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
               <Image
                 src={profilePic}
                 alt="kk"
-                className="w-full h-auto lg:hidden md:inline-block md:w-full"
+                className="w-full h-auto lg:hidden md:inline-block md:w-full md:mb-4"
                 priority
                 sizes="(max-width: 768px) 100vw, (max-width:1200px) 50vw, 33vw"
               />
